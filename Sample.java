2092d4102f9e369f9e4085707800cdf06f6718a3
KishoreMunagala <147137224+KishoreMunagala@users.noreply.github.com>
2024-08-25 14:33:37 -0700

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,7 +1,46 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
+// Time Complexity :O(m+n)
+// Space Complexity :o(1)
+// Did this code successfully run on Leetcode :yes
 // Any problem you faced while coding this :
 
 
 // Your code here along with comments explaining your approach
+
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) 
+    {
+        int lenA = 0;
+        ListNode curr = headA;
+        while(curr != null)
+        {
+            curr = curr.next;
+            lenA++; 
+        }
+
+        int lenB = 0;
+        curr = headB;
+        while(curr != null)
+        {
+            curr = curr.next;
+            lenB++;
+        }
+
+        while(lenA>lenB)
+        {
+            headA = headA.next;
+            lenA--;
+        }
+        while(lenB>lenA)
+        {
+            headB = headB.next;
+            lenB--;
+        }
+
+        while(headA != headB)
+        {
+            headA = headA.next;
+            headB = headB.next;
+        }
+    return headB;
+    }
+}
